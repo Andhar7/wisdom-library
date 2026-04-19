d9c90b57b8343974604143e01ae74eaddefbf780
Andhar7 <boxingprofi@web.de>
2026-04-19 19:55:01 +0200

    Day 4: Author model with migrations

--- library/models.py
+++ library/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
