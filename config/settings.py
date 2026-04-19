20b9d28e81bbea7e2b5aad36822aab941d818a5a
Andhar7 <boxingprofi@web.de>
2026-04-19 19:06:00 +0200

    Day 3: create library app and register in INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "library.apps.LibraryConfig",
 ]
 
 MIDDLEWARE = [
@@ -116,4 +117,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-
