36e0b3539b6203de386900cafe4de9bbdee81942
Andhar7 <boxingprofi@web.de>
2026-04-21 23:07:57 +0200

    Claude and me Day_5. Created Book and Category models. Note: ManyToManyField created one more Table. Check: python manage.py dbshell - .tables

--- library/models.py
+++ library/models.py
@@ -8,3 +8,26 @@ class Author(models.Model):
 
     def __str__(self) -> str:
         return str(self.name)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    categories = models.ManyToManyField(Category, related_name="books", blank=True)
+    description = models.TextField(blank=True)
+    published_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.title)
