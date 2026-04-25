7c60bd7011f2ac6ce6805e8ac43c594650492b92
Andhar7 <boxingprofi@web.de>
2026-04-25 19:56:32 +0200

    Day 10: first view — book_list returns JSON, URL wired

--- library/views.py
+++ library/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Book
 
 # Create your views here.
+
+
+def book_list(request):
+    books = Book.objects.select_related("author").all()
+    data = [
+        {
+            "id": book.id,
+            "title": book.title,
+            "author": book.author.name,
+        }
+        for book in books
+    ]
+    return JsonResponse({"books": data})
