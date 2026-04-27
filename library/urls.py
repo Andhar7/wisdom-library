6954a7b27a6e7d05e4de773bb5b02cc28fe9c14a
Andhar7 <boxingprofi@web.de>
2026-04-27 19:10:01 +0200

    Day 12: book detail, book create with POST, PRG pattern, prettierignore

--- library/urls.py
+++ library/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 app_name = "library"
 
-urlpatterns = [path("", views.book_list, name="book_list")]
+urlpatterns = [
+    path("", views.book_list, name="book_list"),
+    path("<int:pk>/", views.book_detail, name="book_detail"),
+    path("create/", views.book_create, name="book_create"),
+]
--- library/views.py
+++ library/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Author, Book
 
 # Create your views here.
 
@@ -8,3 +8,24 @@ def book_list(request):
     books = Book.objects.select_related("author").all()
 
     return render(request, "library/book_list.html", {"books": books})
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    return render(request, "library/book_list.html", {"book": book})
+
+
+def book_create(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        author_id = request.POST["author_id"]
+        author = get_object_or_404(Author, pk=author_id)
+
+        Book.objects.create(title=title, author=author)
+
+        return redirect("library:book_list")
+
+    authors = Author.objects.all()
+
+    return render(request, "library/book_create.html", {"authors": authors})
